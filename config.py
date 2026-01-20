a4c09d9558d84793722049c99bec3b77cce2b356
Rulf <lazaarx9@gmail.com>
2026-01-20 12:47:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "de232ffdec4d8b0988375398d0083a73")
     BOT_TOKEN = environ.get("BOT_TOKEN", "8217671198:AAEA_ocumXdiEAdyXVXwGQMdtZV1-h59uKc") 
     BOT_SESSION = environ.get("BOT_SESSION", "SteveBotz")
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Ramzy:Ramzy@cluster0.od4yk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://zee:zee@cluster0.s5dgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER = int(environ.get("BOT_OWNER", "5179011789"))
     
@@ -15,6 +15,7 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
+
 
 
 
