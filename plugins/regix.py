01e69a8e3b79862fb2799c07d4bdff1d13219a8e
Rulf <lazaarx9@gmail.com>
2026-01-20 12:26:14 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -392,7 +392,7 @@ async def restart_pending_forwads(bot, user):
     user = user['user_id']
     settings = await db.get_forward_details(user)
     try:
-       skiping = max(0, settings['offset'] - 4)
+       skiping = settings['offset']
        fetch = settings['fetched'] - settings['skip']
        temp.forwardings += 1
        forward_id = await store_vars(user)
